43db2ba71dd582ece9608974d99c2e358cc4f918
Ezequiel Nascimento Campos <ezequiel.campos@fgv.br>
2023-01-30 18:27:32 -0300

    versão 1.0.1- Gera um arquivo .xlsx com o resultado de uma imagem

--- rekognition.py
+++ rekognition.py
@@ -1,8 +1,10 @@
 import csv
 from urllib import response
+# biblioteca que se conecta com a AWS
 import boto3
+import openpyxl
 
-# cria um cliente da aws lendo as credenciais do arquivo credentials.csv
+# Cria um cliente da AWS lendo as credenciais do arquivo credentials.csv
 with open('C:/Users/ezequ/Desktop/UNICARIOCA/1TCC/AWS/credentials.csv', 'r') as input:
     next(input)
     reader = csv.reader(input)
@@ -10,22 +12,42 @@ with open('C:/Users/ezequ/Desktop/UNICARIOCA/1TCC/AWS/credentials.csv', 'r') as 
         access_key_id = line[2]
         secret_access_key = line[3]
 
-#usa a foto
+# Usa a foto
 photo = "C:/Users/ezequ/Desktop/UNICARIOCA/1TCC/AWS/desenho_carro.jpg"
 
-#cria o cliente
-client=boto3.client('rekognition',
-                    aws_access_key_id=access_key_id,
-                    aws_secret_access_key=secret_access_key,
-                    region_name='us-east-1')
+# Cria o cliente
+client = boto3.client('rekognition',
+                      aws_access_key_id=access_key_id,
+                      aws_secret_access_key=secret_access_key,
+                      region_name='us-east-1')
 
-#converte uma imagem em um array de bytes
+# Converte uma imagem em um array de bytes
 with open(photo, 'rb') as source_image:
     source_bytes = source_image.read()
 
-#usa o cliente para detectar as labels(camadas, o quão longe ele pode se aprofundar no reconhecimento)
+# Usa o cliente para detectar as labels (camadas, o quão longe ele pode se aprofundar no reconhecimento)
 response = client.detect_labels(Image={'Bytes': source_bytes},
                                 MaxLabels=2,
                                 MinConfidence=95)
 
+# Imprime o resultado
 print(response)
+
+# Cria ou abre o arquivo .xlsx para escrita
+workbook = openpyxl.Workbook()
+worksheet = workbook.active
+
+# Escreve os cabeçalhos das colunas
+worksheet.cell(row=1, column=1, value="Label (Rótulo)")
+worksheet.cell(row=1, column=2, value="Confidence (Confiança) em %")
+
+# Percorre as labels detectadas e escreve os resultados na planilha
+for i, label in enumerate(response["Labels"]):
+    worksheet.cell(row=i+2, column=1, value=label["Name"])
+    worksheet.cell(row=i+2, column=2, value=label["Confidence"])
+
+# Salva o arquivo .xlsx
+workbook.save("resultado.xlsx")
+
+
+
